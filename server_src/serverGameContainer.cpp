7ff7dec7e41c94348385e1f99fa044502d002883
manulon <mlongoe@fi.uba.ar>
2021-06-09 10:47:59 -0300

    Arregle #41

--- server_src/serverGameContainer.cpp
+++ server_src/serverGameContainer.cpp
@@ -6,25 +6,35 @@ GameContainer:: GameContainer():
 game_finished(false){}
 
 bool GameContainer:: contains(const std::string& key) {
-    return this->map.contains(key);
+    return this->internal.find(key) != this->internal.end();
 }
 
 TaTeTi* GameContainer:: create_new_game(const std::string& key){
     std::unique_lock<std::mutex> lk(this->m);
     if (!contains(key)) {
-        return this->map.add_new_game(new TaTeTi(key));
+        TaTeTi* game = new TaTeTi(key);
+        this->internal[key] = game;
+        return game;
     }
     return NULL;
 }
 
 std::string GameContainer::get_all_values(){
     std::unique_lock<std::mutex> lk(this->m);
-    return this->map.get_all_values();
+    std::string all_games_name("Partidas : \n");
+    for (auto& element: internal){
+        all_games_name += ("- " + element.second->get_name() + "\n");
+    }
+    return all_games_name;
 }
 
 TaTeTi* GameContainer::get_game(const std::string& key){
     std::unique_lock<std::mutex> lk(this->m);
-    return this->map.get_game(key);
+    return this->internal.at(key);
 }
 
-GameContainer:: ~GameContainer(){}
+GameContainer:: ~GameContainer(){
+    for (auto &element : internal) {
+        delete element.second;
+	}
+}
--- server_src/serverGameContainer.h
+++ server_src/serverGameContainer.h
@@ -1,7 +1,8 @@
 #ifndef SERVER_GAME_CONTAINER_H
 #define SERVER_GAME_CONTAINER_H
 
-#include "serverProtectedMap.h"
+#include "TaTeTi.h"
+#include <map>
 #include <mutex>
 #include <condition_variable>
 #include <atomic>
@@ -9,24 +10,24 @@
 
 class GameContainer{
 private:
-    ProtectedMap map;
+    std::map<std::string, TaTeTi*> internal;
     std::mutex m;
     std::condition_variable cv;
     std::atomic<bool> game_finished;
     
-    //Delega la busqueda de una key en particular al mapa.
+    //Busca una key en particular.
     bool contains(const std::string& key);
 
 public:
     GameContainer();
     ~GameContainer();
     
-    //Crea un nuevo juego, delega la inclusion de esta al
+    //Crea un nuevo juego, lo incluye al
     //mapa y luego lo devuelve.
     //Pre: La key, que representa al nombre del juego, es valida.
     TaTeTi* create_new_game(const std::string& key);
 
-    //Delega la obtencion de todos los valores del mapa.
+    //Obtiene todos los valores del mapa.
     std::string get_all_values();
 
     //Devuelve un puntero al juego de nombre key.
